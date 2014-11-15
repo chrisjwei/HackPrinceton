3349410d322bf53de6f13dc1f803f493ab873ef0
Chris Wei <cwei1994@gmail.com>
2014-11-15 05:21:01 -0500

    stuff

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function scroll_to_div()
 function make_map(){
     // copy-paste this tag: <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
     // map center
+
     var myLatlng = new google.maps.LatLng(25.6586, -80.3568);
     // map options,
     var myOptions = {
@@ -17,5 +18,10 @@ function make_map(){
           scaleControl: true
     };
         // standard map
-    map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+    map = new google.maps.Map(document.getElementById("map"), myOptions);
+    console.log(map);
 }
+
+$( document ).ready(function() {
+  make_map();
+});
