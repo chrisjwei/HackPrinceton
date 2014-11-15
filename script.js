59881608906a01d9f2e750c6b73d4987efa06208
Anatol Liu <axl@andrew.cmu.edu>
2014-11-15 04:20:46 -0500

    uploaded maps script

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function scroll_to_div()
     scrollTop: $("#anchor").offset().top
  	});
 }
+function make_map(){
+    // copy-paste this tag: <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+    // map center
+    var myLatlng = new google.maps.LatLng(25.6586, -80.3568);
+    // map options,
+    var myOptions = {
+          zoom: 4,
+          center: myLatlng,
+          panControl: false,
+          zoomControl: false,
+          scaleControl: true
+    };
+        // standard map
+    map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+}
